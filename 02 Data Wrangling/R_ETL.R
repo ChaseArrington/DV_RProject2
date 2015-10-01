3e591a8d13d6e42055b1a877f88855c65de51f20
Chase Arrington <chasearrington1@gmail.com>
2015-10-01 16:03:06 -0500

    project 2

--- 02 Data Wrangling/R_ETL.R
+++ 02 Data Wrangling/R_ETL.R
@@ -9,3 +9,11 @@ file_path <- "Batting.csv"
 measures <- c("yearID","stint","G","AB","R","H","2B","3B","HR","RBI","SB","CS","BB","SO","IBB","HBP","SH","SF","GIDP")
 
 df <- read.csv(file_path, stringsAsFactors = FALSE)
+
+str(df)
+View(df)
+write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName)
+sql <- paste(sql, ");")
+cat(sql)
